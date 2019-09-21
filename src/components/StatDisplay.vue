86053cf54d383f688e046f28f0749a922ada705e
SquareScripter <prevosto.g@sfr.fr>
2019-09-21 22:40:57 +0200

    Pretty stats

--- src/components/StatDisplay.vue
+++ src/components/StatDisplay.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="unit-display">
-    <div key="stats-grid" v-if="unit">
-      <div key="stat">{{stat}} : {{statValue}}</div>
+  <div :class="darkStyle?'stat-display-d':'stat-display'" style="width:14em; height:1.2em;">
+    <div key="stats-grid">
+      <div class="stat-name">{{stat}}</div>
+      <div class="stat-value" v-if="statBar">{{statValue}}</div>
+      <div class="stat-value" style="width: 85%; text-align: center" v-else>{{statValue}}</div>
+      <div class="bar-bg" v-if="statBar">
+        <div class="bar-fg" :style="{width: statValue*2+'%'}"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,10 +14,11 @@
 <script>
 export default {
   name: "StatDisplay",
-  props: { unit: Object, stat: String },
+  props: { unit: Object, stat: String, statBar: Boolean, darkStyle: Boolean },
   computed: {
     statValue: function() {
-      return this.unit[this.stat];
+      if (this.unit) return this.unit[this.stat];
+      else return 0;
     }
   }
 };
@@ -20,4 +26,51 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.stat-display {
+  margin: 2px;
+  border-bottom: solid;
+  border-width: 1px;
+  border-color: rgba(0, 0, 0, 0.5);
+  padding: 2px;
+}
+.stat-display-d {
+  margin: 2px;
+  border: solid;
+  border-width: 2px;
+  border-color: rgb(80, 75, 60);
+  padding: 2px;
+}
+.stat-display-d > div {
+  color: white;
+  height: 1em;
+  padding: 2px;
+  background: rgb(80, 75, 60);
+}
+.stat-name {
+  float: left;
+  text-align: left;
+  width: 2em;
+}
+.stat-value {
+  float: right;
+  text-align: right;
+  width: 1em;
+  font-weight: bold;
+}
+.bar-bg {
+  width: 6em;
+  background: rgba(0, 0, 0, 0.25);
+  border-style: solid;
+  border-width: 1px;
+  border-color: rgba(0, 0, 0, 0.25);
+  display: inline-block;
+}
+.bar-fg {
+  height: 10px;
+  background-image: linear-gradient(
+    to right,
+    rgb(185, 185, 250),
+    rgb(200, 175, 250)
+  );
+}
 </style>
--- src/components/UnitDisplay.vue
+++ src/components/UnitDisplay.vue
@@ -3,16 +3,18 @@
     <h1 v-if="unit">{{unit.Name}}</h1>
     <h1 v-else>UNDEFINED</h1>
     <div id="stats-grid">
-      <div>Lv 1</div>
-      <StatDisplay key="hp" :unit="unit" stat="HP"/>
-      <StatDisplay key="str" :unit="unit" stat="STR"/>
-      <StatDisplay key="mag" :unit="unit" stat="Mag"/>
-      <StatDisplay key="dex" :unit="unit" stat="Dex"/>
-      <StatDisplay key="spd" :unit="unit" stat="Spd"/>
-      <StatDisplay key="lck" :unit="unit" stat="Lck"/>
-      <StatDisplay key="def" :unit="unit" stat="Def"/>
-      <StatDisplay key="res" :unit="unit" stat="Res"/>
-      <StatDisplay key="chm" :unit="unit" stat="Chm"/>
+      <StatDisplay key="lv" :unit="unit" stat="Lv"/>
+      <div/>
+      <StatDisplay key="hp" :unit="unit" stat="HP" darkStyle/>
+      <StatDisplay key="mv" :unit="unit" stat="Mv"/>
+      <StatDisplay key="str" :unit="unit" stat="STR" statBar/>
+      <StatDisplay key="mag" :unit="unit" stat="Mag" statBar/>
+      <StatDisplay key="dex" :unit="unit" stat="Dex" statBar/>
+      <StatDisplay key="spd" :unit="unit" stat="Spd" statBar/>
+      <StatDisplay key="lck" :unit="unit" stat="Lck" statBar/>
+      <StatDisplay key="def" :unit="unit" stat="Def" statBar/>
+      <StatDisplay key="res" :unit="unit" stat="Res" statBar/>
+      <StatDisplay key="chm" :unit="unit" stat="Chm" statBar/>
     </div>
   </div>
 </template>
@@ -33,7 +35,7 @@ export default {
 <style scoped>
 #stats-grid {
   display: grid;
-  grid-template-columns: auto auto;
+  grid-template-columns: 16em 16em;
   grid-template-rows: auto auto auto auto auto;
 }
 </style>
